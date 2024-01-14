e49da66bea6d1af249cd69a3f7604979dc4b897a
Pranav Ramachandra <pranavramachandra@pranavs-air.lan>
2024-01-13 18:16:10 -0600

    added calendar to home page

---
+++ website/make_calendar.py
@@ -0,0 +1,60 @@
+from datetime import datetime, timedelta
+
+
+calendar_dict = {"Sunday": 0,
+                "Monday": 1,
+                "Tuesday": 2,
+                "Wednesday": 3,
+                "Thursday": 4,
+                "Friday": 5,
+                "Saturday": 6
+                }
+
+def get_first_day_of_the_month(dt):
+    orig_dt = dt
+    while dt.month == orig_dt.month:
+        dt = dt - timedelta(days = 1)
+    return dt + timedelta(days = 1)
+
+def generate_week(dt_list, dt):
+    orig_dt = dt
+    if dt_list == []:
+        dt = dt + timedelta(days = 1)
+        while len(dt_list) < 7 and dt.month == orig_dt.month:
+            dt_list.append(Day(dt = dt, completed_workouts=[]))
+            dt = dt + timedelta(days = 1)
+    else:
+        while (dt.strftime("%A") != 'Sunday' or dt_list == []) and dt.month == orig_dt.month:
+            dt_list.append(Day(dt = dt, completed_workouts= []))
+            dt = dt + timedelta(days = 1)
+    return dt_list, dt_list[-1].dt
+
+def generate_calendar(dt):
+    calendar, orig_dt = [], dt
+    current_day = get_first_day_of_the_month(dt)
+    first_week = [None for d in range(calendar_dict[current_day.strftime("%A")])]
+    first_week, current_day = generate_week(first_week, current_day)
+    calendar.append(first_week)
+
+    while (current_day + timedelta(days = 1)).month == orig_dt.month:
+        new_week, current_day = generate_week([], current_day)
+        while len(new_week) < 7:
+            new_week.append(None)
+        calendar.append(new_week)
+
+    return calendar
+
+class Calendar:
+
+    def __init__(self, user_id, dt) -> None:
+        self.dt = dt
+        self.user_id = user_id
+        self.calendar = generate_calendar(dt = dt)
+
+class Day:
+    def __init__(self, completed_workouts, dt) -> None:
+        self.dt = dt
+        self.completed_workouts = completed_workouts
+    
+    def __repr__(self) -> str:
+        return f'{self.dt.strftime("%A")}, {self.dt.month} {self.dt.day}'
--- website/views.py
+++ website/views.py
@@ -1,16 +1,24 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
-from .models import Note
+from .models import *
 from . import *
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
+from .make_calendar import *
 
 views = Blueprint('views', __name__)
-
 
 @views.route('/', methods = ['GET', 'POST'])
 @login_required
 def home():
+    current_month = datetime.now().strftime("%B")
+
+    calendar = Calendar(current_user.id, datetime.now())
+
+    workout_data = WorkoutData.query.filter_by(user_id = current_user.id).all()
+
+    
+
     if request.method == 'POST':
         note = request.form.get('note')
         if len(note) < 1:
@@ -20,7 +28,7 @@ def home():
             db.session.add(new_note)
             db.session.commit()
             flash('Note added!', category = 'success')
-    return render_template("home.html", user = current_user)
+    return render_template("home.html", user = current_user, month = current_month, calendar = calendar)
 
 @views.route('/delete-note', methods = ['POST'])
 @login_required
@@ -33,3 +41,17 @@ def delete_note():
             db.session.delete(note)
             db.session.commit()
     return jsonify({})
+
+def get_current_month_data(workout_data, calendar):
+    return [wd for wd in workout_data if wd.date.month == calendar.dt.month]
+
+def add_workouts_to_dates(workout_data, calendar):
+
+    workout_dict = {} # Keys: num. day of the month, Values: Name of workout
+
+    for w in workout_data:
+        if w.date.month == calendar.dt.month:
+            workout_dict[w.date.day] = w.workout_name
+
+    return workout_dict
+
